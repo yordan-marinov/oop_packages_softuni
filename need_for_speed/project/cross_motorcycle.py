3c13cc6f00cea36fff1f259ccbd9d1345c6f13fe
yordan-marinov <jordanmarinov8@gmail.com>
2020-12-30 19:52:21 +0200

    Added new project: 'need_for_speed'

---
+++ need_for_speed/project/cross_motorcycle.py
@@ -0,0 +1,5 @@
+from need_for_speed.project.motorcycle import Motorcycle
+
+
+class CrossMotorcycle(Motorcycle):
+    pass
---
+++ need_for_speed/project/sport_car.py
@@ -0,0 +1,5 @@
+from need_for_speed.project.car import Car
+
+
+class SportCar(Car):
+    DEFAULT_FUEL_CONSUMPTION = 10.0
