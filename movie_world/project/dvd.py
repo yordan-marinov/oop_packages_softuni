39a771ccf6f294338126bfa4c61b495c5462b0d2
yordan-marinov <jordanmarinov8@gmail.com>
2021-02-21 19:19:14 +0200

    Updated package: 'movie_world'

--- movie_world/project/dvd.py
+++ movie_world/project/dvd.py
@@ -1,11 +1,28 @@
+from datetime import datetime
+
+
 class DVD:
-    def __init__(self, name: str, id: int, creation_year: int, creation_month: str, age_restriction: int):
+    def __init__(
+        self,
+        name: str,
+        id: int,
+        creation_year: int,
+        creation_month: str,
+        age_restriction: int,
+    ):
         self.name: str = name
         self.id: int = id
         self.creation_year: int = creation_year
         self.creation_month: str = creation_month
         self.age_restriction: int = age_restriction
-        self.is_rented: bool = False
+        self.is_rented = False
+
+    @classmethod
+    def from_date(cls, id: int, name: str, date: str, age_restriction: int):
+        date_object = datetime.strptime(date, "%d.%m.%Y").date()
+        date_str = datetime.strftime(date_object, "%d %B %Y")
+        _ , month, year = date_str.split()
+        return cls(name, id, int(year), month, age_restriction)
 
     def is_rented_dvd_repr(self):
         if self.is_rented:
@@ -13,37 +30,5 @@ class DVD:
         return "not rented"
 
     def __repr__(self):
-        return f"{self.id}: {self.name} " \
-               f"({self.creation_month} {self.creation_year}) " \
-               f"has age restriction {self.age_restriction}. " \
-               f"Status: {self.is_rented_dvd_repr()}"
-
-    @staticmethod
-    def string_repr_of_month(month) -> str:
-        months = {
-            "1": "January",
-            "2": "February",
-            "3": "March",
-            "4": "April",
-            "5": "May",
-            "6": "June",
-            "7": "July",
-            "8": "August",
-            "9": "September",
-            "10": "October",
-            "11": "November",
-            "12": "December"
-        }
-        return months[month]
-
-    @staticmethod
-    def split_date(date: str):
-        date = date.split(".")
-        creation_year_from_split = int(date[2])
-        creation_month_from_split = DVD.string_repr_of_month(date[1])
-        return creation_year_from_split, creation_month_from_split
-
-    @classmethod
-    def from_date(cls, id: int, name: str, date: str, age_restriction: int):
-        creation_year, creation_moth = cls.split_date(date)
-        return cls(name, id, creation_year, creation_moth, age_restriction)
+        return f"{self.id}: {self.name} ({self.creation_month} {self.creation_year}) has age restriction {self.age_restriction}. Status: {self.is_rented_dvd_repr()}"
+     
--- movie_world/project/movie_world.py
+++ movie_world/project/movie_world.py
@@ -1,8 +1,12 @@
+from project.customer import Customer
+from project.dvd import DVD
+
+
 class MovieWorld:
     def __init__(self, name: str):
         self.name: str = name
-        self.customers = []  # list of Customers obj.
-        self.dvds = []  # list of DVDs obj.
+        self.customers: [Customer] = []
+        self.dvds: [DVD] = []
 
     @staticmethod
     def dvd_capacity():
@@ -12,60 +16,48 @@ class MovieWorld:
     def customer_capacity():
         return 10
 
-    def add_customer(self, customer):  # customer: Customer(obj)
+    def add_customer(self, customer: Customer):
         if len(self.customers) < MovieWorld.customer_capacity():
             self.customers.append(customer)
 
-    def add_dvd(self, dvd):
+    def add_dvd(self, dvd: DVD):
         if len(self.dvds) < MovieWorld.dvd_capacity():
             self.dvds.append(dvd)
 
     def rent_dvd(self, customer_id: int, dvd_id: int):
-        customer = self.get_customer_by_id(customer_id)
-        dvd = self.get_dvd_by_id(dvd_id)
-        if dvd in customer.rented_dvds:
-            return f"{customer.name} has already rented {dvd.name}"
+        dvd_obj = [d for d in self.dvds if d.id == dvd_id][0]
+        customer_obj = [c for c in self.customers if c.id == customer_id][0]
+        
+        if dvd_obj in customer_obj.rented_dvds:
+            return f"{customer_obj.name} has already rented {dvd_obj.name}"
 
-        if customer.age < dvd.age_restriction:
-            return f"{customer.name} should be at least " \
-                   f"{dvd.age_restriction} to rent this movie"
+        if customer_obj.age < dvd_obj.age_restriction:
+            return f"{customer_obj.name} should be at least {dvd_obj.age_restriction} to rent this movie"
 
-        if dvd.is_rented:
+        if dvd_obj.is_rented:
             return f"DVD is already rented"
 
-        customer.rented_dvds.append(dvd)
-        dvd.is_rented = not dvd.is_rented
+        dvd_obj.is_rented = not dvd_obj.is_rented
+        customer_obj.rented_dvds.append(dvd_obj)
+        
+        return f"{customer_obj.name} has successfully rented {dvd_obj.name}"
 
-        return f"{customer.name} has successfully rented {dvd.name}"
-
-    def return_dvd(self, customer_id, dvd_id):
-        customer = self.get_customer_by_id(customer_id)
-        dvd = self.get_dvd_by_id(dvd_id)
-
-        if dvd in customer.rented_dvds:
-            customer.rented_dvds.remove(dvd)
-            dvd.is_rented = not dvd.is_rented
-            return f"{customer.name} has successfully returned {dvd.name}"
-
-        return f"{customer.name} does not have that DVD"
+    def return_dvd(self, customer_id: int, dvd_id: int):
+        customer_obj = [c for c in self.customers if c.id == customer_id][0]
+        dvd_obj = [d for d in self.dvds if d.id == dvd_id][0]
+        
+        if dvd_obj in customer_obj.rented_dvds:
+            dvd_obj.is_rented = False
+            customer_obj.rented_dvds.remove(dvd_obj)
+            return f"{customer_obj.name} has successfully returned {dvd_obj.name}"
+        
+        return f"{customer_obj.name} does not have that DVD"
 
     def __repr__(self):
         result = ""
         for customer in self.customers:
-            result += customer.__repr__ + "\n"
+            result += f"{customer}\n"
         for dvd in self.dvds:
-            result += dvd.__repr__ + "\n"
-
+            result += f"{dvd}\n"
+        
         return result
-
-    def get_customer_by_id(self, customer_id):
-        customer = [
-            customer
-            for customer in self.customers
-            if customer.id == customer_id
-        ][0]
-        return customer
-
-    def get_dvd_by_id(self, dvd_id):
-        dvd = [dvd for dvd in self.dvds if dvd.id == dvd_id][0]
-        return dvd
