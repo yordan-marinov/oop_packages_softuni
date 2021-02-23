62c8eba89922da9dfc751a3c29f070433aeb7bf1
yordan-marinov <jordanmarinov8@gmail.com>
2021-02-23 15:43:32 +0200

    Updated package: 'guild'

--- guild_system/project/guild.py
+++ guild_system/project/guild.py
@@ -1,44 +1,26 @@
-# from guild_system.project.player import Player
-# from typing import List
-
-
 class Guild:
-    # name: str
-    # list_of_players: List[Player]
-
-    def __init__(self, name: str, ):
+    def __init__(self, name):
         self.name = name
         self.players = []
 
     def assign_player(self, player):
-        if player.guild != "Unaffiliated" and player.guild != self.name:
-            return f"{player.name} is in another guild."
+        if not player.guild == "Unaffiliated" and not player.guild == self.name:
+            return f"Player {player.name} is in another guild."
+        elif player.guild == self.name:
+            return f"Player {player.name} is already in the guild."
+        else:
+            self.players.append(player)
+            player.guild = self.name
+            return f"Welcome player {player.name} to the guild {self.name}"
 
-        if player.name in [p.name for p in self.players]:
-            return f"Player {player.name} is already in the guild"
-
-        self.players.append(player)
-        player.guild = self.name
-
-        return f"Welcome player {player.name} to the guild {self.name}"
-
-    def kick_player(self, player_name: str):
-        if player_name not in [p.name for p in self.players]:
-            return f"Player {player_name} in not in the guild."
-
-        current_player = [
-            p for p in self.players
-            if player_name == p.name
-        ].pop()
-
-        self.players.remove(current_player)
-        current_player.guild = "Unaffiliated"
-
-        return f"Player {current_player.name} has been removed from the guild."
+    def kick_player(self, player_name):
+        if player_name not in map(lambda x: x.name, self.players):
+            return f"Player {player_name} is not in the guild."
+        self.players.remove(next(filter(lambda x: x.name == player_name, self.players)))
+        return f"Player {player_name} has been removed from the guild."
 
     def guild_info(self):
-        info = f"Guild: {self.name}\n"
-        for players in self.players:
-            info += players.player_info()
-
-        return info
+        data = f"Guild: {self.name}\n"
+        for p in self.players:
+            data += p.player_info()
+        return data
--- guild_system/project/player.py
+++ guild_system/project/player.py
@@ -1,5 +1,4 @@
 class Player:
-
     def __init__(self, name: str, hp: int, mp: int):
         self.name = name
         self.hp = hp
@@ -9,14 +8,13 @@ class Player:
 
     def add_skill(self, skill_name, mana_cost):
         if skill_name in self.skills:
-            return "Skill already added"
-
+            return f"Skill already added"
         self.skills[skill_name] = mana_cost
         return f"Skill {skill_name} added to the collection of the player {self.name}"
 
     def player_info(self):
-        player_data = f"Name: {self.name}\nGuild: {self.guild}\nHP: {self.hp}\nMP: {self.mp}\n"
-        for skill_name, skill_value in self.skills.items():
-            player_data += f"==={skill_name} - {skill_value}\n"
+        data = f"Name: {self.name}\nGuild: {self.guild}\nHP: {self.hp}\nMP: {self.mp}\n"
+        for s, m in self.skills.items():
+            data += f"==={s} - {m}\n"
+        return data
 
-        return player_data
