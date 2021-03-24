5f0553d2449868b8d53f99a6e79f1f5f2fd1253b
yordan-marinov <jordanmarinov8@gmail.com>
2021-03-24 19:15:06 +0200

    Updated package: 'pokemon'

--- pokemon/main.py
+++ pokemon/main.py
@@ -2,7 +2,48 @@ from project.pokemon import Pokemon
 from project.trainer import Trainer
 
 
+def test_pokemon_init():
+    p = Pokemon("Jordan", 10)
+    print(p.name)
+    print(p.health)
+    print(p.pokemon_details())
+
+def test_trainer_init():
+    t = Trainer("Martin")
+    print(t.name)
+    print(t.pokemon)
+
+def test_trainer_add_pokemon():
+    t = Trainer("Martin")
+    p = Pokemon("Jordan", 10)
+    print(t.add_pokemon(p))
+    print(t.pokemon)
+
+def test_trainer_add_pokemon_already_in():
+    t = Trainer("Martin")
+    p = Pokemon("Jordan", 10)
+    t.add_pokemon(p)
+    print(t.add_pokemon(p))
+    print(t.pokemon)
+    
+def test_trainer_release_pokemon_already_in():
+    t = Trainer("Martin")
+    p = Pokemon("Jordan", 10)
+    t.add_pokemon(p)
+    print(t.release_pokemon("Jordan"))
+
+def test_trainer_release_pokemon_not_in():
+    t = Trainer("Martin")
+    print(t.release_pokemon("Jordan"))
+
+
 if __name__ == "__main__":
+    # test_pokemon_init()
+    # test_trainer_init()
+    # test_trainer_add_pokemon()
+    # test_trainer_add_pokemon_already_in()
+    # test_trainer_release_pokemon_already_in()
+    # test_trainer_release_pokemon_not_in()
     pokemon = Pokemon("Pikachu", 90)
     print(pokemon.pokemon_details())
     trainer = Trainer("Ash")
--- pokemon/project/trainer.py
+++ pokemon/project/trainer.py
@@ -2,31 +2,31 @@ from project.pokemon import Pokemon
 
 
 class Trainer:
-    # name: str
-    # pokemon: List[Pokemon]
-
     def __init__(self, name: str):
-        self.name = name
-        self.pokemon = []
+        self.name: str = name
+        self.pokemon: [Pokemon] = []
 
     def add_pokemon(self, pokemon: Pokemon):
         if pokemon in self.pokemon:
             return f"This pokemon is already caught"
-
         self.pokemon.append(pokemon)
         return f"Caught {pokemon.pokemon_details()}"
-
+    
+    def get_pokemon_by_name(self, name):
+        obj = [p for p in self.pokemon if name == p.name]
+        if obj:
+            return obj[0]
+    
     def release_pokemon(self, pokemon_name: str):
-        searched_pokemon = [p for p in self.pokemon if pokemon_name == p.name]
-        if not searched_pokemon:
-            return "Pokemon is not caught"
-
-        self.pokemon.remove(searched_pokemon[0])
+        if not self.get_pokemon_by_name(pokemon_name):
+            return f"Pokemon is not caught"
+        self.pokemon.remove(self.get_pokemon_by_name(pokemon_name))
         return f"You have released {pokemon_name}"
-
+    
     def trainer_data(self):
-        result = f"Pokemon Trainer {self.name}\nPokemon count {len(self.pokemon)}\n"
-        for p in self.pokemon:
-            result += f"- {p.pokemon_details()}\n"
-
-        return result
+        s = ""
+        s += f"Pokemon Trainer {self.name}\n"
+        s += f"Pokemon count {len(self.pokemon)}\n"
+        for pokemon in self.pokemon:
+            s += f"- {pokemon.pokemon_details()}\n"
+        return s.strip()
